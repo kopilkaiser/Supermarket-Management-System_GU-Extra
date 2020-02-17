aafab85d83b3a2d54eb978ea2617496743c162e9
p17244806 <p17244806@GWHXX472WIN007W>
2020-02-17 21:50:18 +0000

    Here i created one test Called tstOrder

---
+++ SupermarketManagementSystem/SMSTestProject/tstOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SMSTestProject
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
