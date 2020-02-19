6b48516c37ea7302543e00187f6e4130b3781243
p17244806 <p17244806@GWHXX472WIN007W>
2020-02-19 22:43:58 +0000

    Testing on Order Price and Quantity

--- SupermarketManagementSystem/ClassLibrary/clsOrder.cs
+++ SupermarketManagementSystem/ClassLibrary/clsOrder.cs
@@ -1,6 +1,18 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public string Valid(int quantity, object price)
+        {
+            // create a variable to store any error message 
+            String Error = "";
+            //if the seleted quantity for the order less than 0 or equal to zero
+           
+        }
     }
 }
--- SupermarketManagementSystem/SMSTestProject/tstOrder.cs
+++ SupermarketManagementSystem/SMSTestProject/tstOrder.cs
@@ -7,12 +7,61 @@ namespace SMSTestProject
     [TestClass]
     public class tstOrder
     {
+        // some good data to pass the method 
+        Int32 OrderQuantity = 1;
+        double OrderPrice = 10.99;
+
         [TestMethod]
         public void InstanceOK()
         {
-            // instance of class Order
+            //create an instance of class Order
             clsOrder AnOrder = new clsOrder();
+            // test to see that it exist 
             Assert.IsNotNull(AnOrder);
         }
+        [TestMethod]
+        public void QuantityPropertyOK()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create some test dat to assign the property
+            Int32 Quantity = 1;
+            // assign the data to the property
+            AnOrder.Quantity = Quantity;
+            //test to see that it exist
+            Assert.AreEqual(AnOrder.Quantity, Quantity);
+            
+        }
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create some test dat to assign the property
+            double Price = 10.99;
+            // assign the data to the property
+            AnOrder.Price = Price;
+            //test to see that it exist
+            Assert.AreEqual(AnOrder.Price, Price);
+
+        }
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            Int32 Quantity = 1;
+            // create some test data to asign the property
+            double Price = 10.99;
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There are no error message 
+            Assert.AreEqual(Error, "");
+
+        }
+
     }
 }
