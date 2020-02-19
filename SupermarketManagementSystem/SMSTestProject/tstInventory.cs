b26eec54021d57e3b19943509891804ee0079fb8
Kopil Kaiser <P17511696@my365.dmu.ac.uk>
2020-02-19 22:43:40 +0000

    Created clsInventory and tstInventory. Wrote test methods in tstInventory for method Valid in clsInventory

--- SupermarketManagementSystem/SMSTestProject/tstInventory.cs
+++ SupermarketManagementSystem/SMSTestProject/tstInventory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SMSTestProject
@@ -6,9 +7,75 @@ namespace SMSTestProject
     [TestClass]
     public class tstInventory
     {
+        //some good test data
+        string Name = "Cadbury";
+        decimal Price = 6.99m;
+        int Quantity = 999;
+        string Category = "Electronics";
+        string DateAdded = DateTime.Now.Date.ToString();
+
         [TestMethod]
-        public void TestMethod1()
+        public void InstanceOK()
         {
+            //create an instance of the class
+            clsInventory AnInventory = new clsInventory();
+            Assert.IsNotNull(AnInventory);
         }
+
+        [TestMethod]
+        public void NamePropertyOK()
+        {
+            clsInventory AnInventory = new clsInventory();
+            string Name = "Cadbury";
+            AnInventory.Name = Name;
+            Assert.AreEqual(Name, AnInventory.Name);
+        }
+
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            clsInventory AnInventory = new clsInventory();
+            decimal Price = 5.99m;
+            AnInventory.Price = Price;
+            Assert.AreEqual(Price, AnInventory.Price);
+        }
+
+        [TestMethod]
+        public void QuantityPropertyOK()
+        {
+            clsInventory AnInventory = new clsInventory();
+            Int32 Quantity = 100;
+            AnInventory.Quantity = Quantity;
+            Assert.AreEqual(Quantity, AnInventory.Quantity);
+        }
+
+        [TestMethod]
+        public void CategoryPropertyOK()
+        {
+            clsInventory AnInventory = new clsInventory();
+            string Category = "Electronics";
+            AnInventory.Category = Category;
+            Assert.AreEqual(Category, AnInventory.Category);
+        }
+
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            clsInventory AnInventory = new clsInventory();
+            string DateAdded = DateTime.Now.Date.ToString();
+            AnInventory.DateAdded = DateAdded;
+            Assert.AreEqual(DateAdded, AnInventory.DateAdded);
+        }
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            clsInventory AnInventory = new clsInventory();
+            string Error = "";
+            Error = AnInventory.Valid(Name, Price, Quantity, Category, DateAdded);
+            Assert.AreEqual(Error, "");
+        }
+
+
     }
 }
