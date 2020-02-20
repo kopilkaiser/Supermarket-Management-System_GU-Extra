661328e4055c20cd7f3b5e9f3a426d7234e526cb
p17244806 <p17244806@GWHXX472WIN007W>
2020-02-20 12:17:58 +0000

    Some changes has been made on tstOrder

--- SupermarketManagementSystem/ClassLibrary/clsOrder.cs
+++ SupermarketManagementSystem/ClassLibrary/clsOrder.cs
@@ -7,12 +7,35 @@ namespace ClassLibrary
         public int Quantity { get; set; }
         public double Price { get; set; }
 
-        public string Valid(int quantity, object price)
+        public string Valid(int quantity, double price)
         {
             // create a variable to store any error message 
             String Error = "";
             //if the seleted quantity for the order less than 0 or equal to zero
-           
+            if (Quantity <= 0)
+            {
+                // store this error message 
+                Error = Error + "The quantity cannot be less than one";
+            }
+            // if the selected quantity exceeds 100
+            if (Quantity >100)
+            {
+                // store this error message 
+                Error = Error + "The selected quantity cannot exceed 100";
+
+            }
+            // if the oder price is less than 0.20 
+            if (Price <= 0.20)
+            {
+                Error = Error + " The price for the order cannot be less than 0.20 ";
+            }
+            // if the order price exceeds 10,000
+            if (Price > 10000)
+            {
+                Error = Error + "the price for the order exceeds the limit ";
+            }
+            // return the error message 
+            return Error;
         }
     }
 }
--- SupermarketManagementSystem/SMSTestProject/tstOrder.cs
+++ SupermarketManagementSystem/SMSTestProject/tstOrder.cs
@@ -8,8 +8,8 @@ namespace SMSTestProject
     public class tstOrder
     {
         // some good data to pass the method 
-        Int32 OrderQuantity = 1;
-        double OrderPrice = 10.99;
+        Int32  Quantity = 1;
+        double Price = 10.99;
 
         [TestMethod]
         public void InstanceOK()
@@ -59,6 +59,231 @@ namespace SMSTestProject
             // invoke the method 
             Error = AnOrder.Valid(Quantity, Price);
             // test to see that result is ok , e, g - There are no error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void QuantityMinLessOne()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            Int32 Quantity = 0;
+            
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There shold be an error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void QuantityMinBoundary()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            Int32 Quantity = 0;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be an error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void QuantityMinPlusOne()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            Int32 Quantity = 2;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be NO error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void QuantityMaxBoundary()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            Int32 Quantity = 100;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be NO error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void QuantityMaxPlusOne()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            Int32 Quantity = 101;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be an error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void QuantityMid()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            Int32 Quantity = 50;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be no error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void QuantityExtremeMax()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            Int32 Quantity = 10000;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be an error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        ///////////////////////////////////////////////////////////
+        [TestMethod]
+        public void PriceMinLessOne()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            double Price = 0;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There shold be an error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void PriceMinBoundary()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            double Price = 0.20;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be an error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void PriceMinPlusOne()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            double Price = 1.20;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be NO error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void PriceMaxBoundary()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+           double Price = 10000;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be NO error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void PriceMaxPlusOne()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            double Price = 10001;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be an error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void PriceMid()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            double Price = 5000;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be no error message 
+            Assert.AreEqual(Error, "");
+
+        }
+        [TestMethod]
+        public void PriceExtremeMax()
+        {
+            //create an instance of class Order
+            clsOrder AnOrder = new clsOrder();
+            // create an string variable to store the result of validation 
+            String Error = "";
+            // create some test data to the test method
+            double Price = 100000;
+
+            // invoke the method 
+            Error = AnOrder.Valid(Quantity, Price);
+            // test to see that result is ok , e, g - There should be an error message 
             Assert.AreEqual(Error, "");
 
         }
