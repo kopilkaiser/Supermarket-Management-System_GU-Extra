1a8db3e2c8f2e08fc3382e9469d6f6f1c28869a9
Kopil Kaiser <kopil_kaiser@hotmail.com>
2020-02-24 19:29:50 +0000

    updated test and class for inventory and inventorycollection

---
+++ SupermarketManagementSystem/ClassLibrary/clsInventoryCollection.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsInventoryCollection
+    {
+        //private data member for the list
+        List<clsInventory> mInventoryList = new List<clsInventory>();
+
+        public clsInventoryCollection()
+        {
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the store procedure
+            DB.Execute("sproc_tblInventory_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank address
+                clsInventory AnInventory = new clsInventory();
+                //read in the fields from the current record
+                AnInventory.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                AnInventory.Name = Convert.ToString(DB.DataTable.Rows[Index]["Name"]);
+                AnInventory.Price = Convert.ToDecimal(DB.DataTable.Rows[Index]["Price"]);
+                AnInventory.Quantity = Convert.ToInt32(DB.DataTable.Rows[Index]["Quantity"]);
+                AnInventory.Category = Convert.ToString(DB.DataTable.Rows[Index]["Category"]);
+                AnInventory.DateAdded = Convert.ToString(DB.DataTable.Rows[Index]["DateAdded"]);
+                //add the record to the private data member
+                mInventoryList.Add(AnInventory);
+                //point at the next record
+                Index++;
+            }
+        }
+        
+
+        //public property for the address list
+        public List<clsInventory> InventoryList
+        {
+            get
+            {
+                //return the private data
+                return mInventoryList;
+            }
+            set
+            {
+                //set the private data
+                mInventoryList = value;
+            }
+        }
+
+        //public property for Count
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mInventoryList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+        public clsInventory ThisInventory { get; set; }
+    }
+}
---
+++ SupermarketManagementSystem/SMSTestProject/tstInventoryCollection.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace SMSTestProject
+{
+    [TestClass]
+    public class tstInventoryCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsInventoryCollection AllInventories = new clsInventoryCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllInventories);
+        }
+
+        [TestMethod]
+        public void InventoryListOK()
+        {
+            //create an instance of the class we want to create
+            clsInventoryCollection AllInventories = new clsInventoryCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsInventory> TestList = new List<clsInventory>();
+            //add an item to the list
+            //create the item of test data
+            clsInventory TestItem = new clsInventory();
+            //set it's properties
+            TestItem.Active = true;
+            TestItem.Name = "Samsung OLED TV";
+            TestItem.Price = 2999.99m;
+            TestItem.Quantity = 100;
+            TestItem.Category = "Electronics";
+            TestItem.DateAdded = DateTime.Now.Date.ToString();
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllInventories.InventoryList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllInventories.InventoryList, TestList);
+        }
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsInventoryCollection AllInventories = new clsInventoryCollection();
+            //create some test data to assign to the property
+            Int32 SomeCount = 2;
+            //assign the data to the property
+            AllInventories.Count = SomeCount;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllInventories.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisInventoryPropertyOK()
+        {
+            //create the instance of the class we want to create
+            clsInventoryCollection AllInventories = new clsInventoryCollection();
+            //create some test data to assign to the property
+            clsInventory TestInventory = new clsInventory();
+            //set the properties of the test object
+            TestInventory.Active = true;
+            TestInventory.Name = "Samsung OLED TV";
+            TestInventory.Price = 2999.99m;
+            TestInventory.Quantity = 100;
+            TestInventory.Category = "Electronics";
+            TestInventory.DateAdded = DateTime.Now.Date.ToString();
+            //assign the data to the property
+            AllInventories.ThisInventory = TestInventory;
+            //test to see that the two values are the same;
+            Assert.AreEqual(AllInventories.ThisInventory, TestInventory);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsInventoryCollection AllInventories = new clsInventoryCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsInventory> TestList = new List<clsInventory>();
+            //add an item to the list
+            //create the item of test data
+            clsInventory TestItem = new clsInventory();
+            //set it's properties
+            TestItem.Active = true;
+            TestItem.Name = "Samsung OLED TV";
+            TestItem.Price = 2999.99m;
+            TestItem.Quantity = 100;
+            TestItem.Category = "Electronics";
+            TestItem.DateAdded = DateTime.Now.Date.ToString();
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllInventories.InventoryList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllInventories.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void TwoRecordsPresent()
+        {
+            //create an instance of the class we want to create
+            clsInventoryCollection AllInventories = new clsInventoryCollection();
+            //test to see that the two values are the same
+            Assert.AreEqual(AllInventories.Count, 2);
+        }
+    }
+}
