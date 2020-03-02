39d198343b75860a46ec41f430a716cea814a7bb
AkborRafi <60204184+AkborRafi@users.noreply.github.com>
2020-03-02 23:58:45 +0000

    Psuh the test plan

---
+++ SupermarketManagementSystem/ClassLibrary/clsPayment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsPayment
+    {
+        
+        public string Method { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+
+        public string Valid(string method, string amount, string paymentDate)
+        {
+            return "";
+        }
+
+        
+    }
+}
